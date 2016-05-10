4b9ecc88dfe28b9679657fff4c31110736168dfa
Suotana <suotana@hitler.rocks>
2016-05-10 13:41:37 +0300

    noni jos nyt toimis

---
+++ diaboloklooni/Assets/scripts/UI/FloatingText.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FloatingText : MonoBehaviour {
+
+
+	public Animator animator;
+	private Text damageText;
+
+	// Use this for initialization
+	void Start () {
+	
+		//animator = animator.GetComponentInChildren <Animator>();
+		AnimatorClipInfo[] clipInfo = animator.GetCurrentAnimatorClipInfo (0);
+		Destroy(gameObject, clipInfo [0].clip.length);
+		damageText = GetComponent<Text> ();
+
+
+	}
+
+	public void SetText(string text){
+	
+		damageText.text = text;
+	
+
+
+	
+	}
+	
+	// Update is called once per frame
+
+}
---
+++ diaboloklooni/Assets/scripts/manabar.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class manabar : MonoBehaviour {
+
+	float healthh;
+	float maxhealthh;
+	//GameObject parentti;
+	GameObject pelaaja;
+	//GameObject kamera;
+	Vector2 n;
+	//Quaternion rota;
+	// Use this for initialization
+	void Start () {
+
+		pelaaja = GameObject.FindGameObjectWithTag ("Player");
+		//parentti = GameObject.Find ("heltit");
+		maxhealthh = pelaaja.GetComponent<Fighter>().maxmana;
+		//kamera = GameObject.Find ("Main Camera");
+		//rota = parentti.transform.rotation;
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+		//n = kamera.transform.position - parentti.transform.position; 
+		healthh = pelaaja.GetComponent<Fighter>().mana;
+		float barlength = healthh / maxhealthh ; 
+		transform.localScale = new Vector3 (barlength, 1f, 1f);
+		//parentti.transform.rotation = Quaternion.LookRotation(n) * Quaternion.Euler(0, 90, -90);
+		//parentti.transform.rotation = rota;
+
+
+	}
+}
